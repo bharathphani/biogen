a265e2cf2691884ba4585d2c682bfa031c4c7c14
Rakesh <rairakesh@gmail.com>
2017-08-04 08:02:24 -0400

    Fix for Multiple Users
    Fix for Multiple Users

---
+++ communityservicesignup/src.main.java/com/src/magnushealth/model/UserWrapper.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.src.magnushealth.model;
+
+import java.util.List;
+
+/**
+ * @author Rakesh Rai
+ *
+ */
+public class UserWrapper {
+	private List<User> users;
+
+	/**
+	 * @return the users
+	 */
+	public List<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * @param users
+	 *            the users to set
+	 */
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+}
